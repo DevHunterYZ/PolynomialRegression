9289e4456ba4b1cc94ece6a670401c9fe692f6a5
DevHunter <32465888+DevHunterYZ@users.noreply.github.com>
2018-07-30 22:02:15 +0300

    Update Polynomregression.py

--- Polynomregression.py
+++ Polynomregression.py
@@ -2,27 +2,28 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
-veri=pd.read_csv('verim.csv')
-x=veri.iloc[:11,0:1]
-y=veri.iloc[:11,1:2]
+veri=pd.read_csv('QBOdata.csv')
+x=veri.iloc[:25,0:1]
+y=veri.iloc[:25,1:2]
 X=x.values
 Y=y.values
 
 from sklearn.linear_model import LinearRegression
 lin_reg=LinearRegression()
 lin_reg.fit(X,Y)
-
+plt.scatter(X,Y,color='red')
 
 from sklearn.preprocessing import PolynomialFeatures
-poly_reg=PolynomialFeatures(degree=6)
+poly_reg=PolynomialFeatures(degree=3)
 x_poly=poly_reg.fit_transform(X)
+print(x_poly)
 
-poly_reg=LinearRegression()
-poly_reg.fit(x_poly,y)
+lin_reg2=LinearRegression()
+lin_reg2.fit(x_poly,y)
 plt.scatter(x,y)
-plt.plot(x,poly_reg.predict(poly_reg.fit_transform(X)),color='blue')
+plt.title('Son 25 aylık QBO değişimi')
+plt.plot(X,lin_reg2.predict(poly_reg.fit_transform(X)),color='green')
 plt.show()
 
-print(poly_reg.predict(poly_reg.transform(11)))
-print(poly_reg.predict(poly_reg.transform(1)))
-print(x_poly)
+print(lin_reg2.predict(poly_reg.fit_transform(25)))
+print(lin_reg2.predict(poly_reg.fit_transform(26)))
